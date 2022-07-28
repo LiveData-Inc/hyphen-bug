6852f7efea61599f37a618c03d8470653a188416
jrobbins-LiveData <jeffr@livedata.com>
2022-07-28 08:40:10 -0400

    initial commit

--- hyphen_bug/hyphen_bug_stack.py
+++ hyphen_bug/hyphen_bug_stack.py
@@ -1,19 +1,32 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import CfnOutput, Stack, aws_lambda
 from constructs import Construct
+
+ALIAS_NAME = 'HYPHEN-BUG'
+
+
+inline_code = """
+def handler(event, context):
+    print(f'{event=} {context=}')
+"""
 
 class HyphenBugStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        handler = aws_lambda.Function(
+            self, 'HyphenBugLambda',
+            code=aws_lambda.InlineCode(inline_code),
+            handler='index.handler',
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HyphenBugQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        alias = handler.add_alias(
+            alias_name=ALIAS_NAME,
+        )
+
+        fn_url = alias.add_function_url(
+            auth_type=aws_lambda.FunctionUrlAuthType.NONE
+        )
+
+        CfnOutput(self, "FunctionUrl", value=fn_url.url)
